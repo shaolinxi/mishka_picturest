0ece0a11932c8e409516fffdb6ed658a0658dd81
xmikex <michal1chmiel@gmail.com>
2016-12-22 16:15:21 +0000

    Cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20161219155221) do
-=======
-ActiveRecord::Schema.define(version: 20161220154708) do
->>>>>>> 2ca7ba5... installed paperclip
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
