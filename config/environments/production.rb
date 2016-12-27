2cd4f41c810f05aa9933f761bc66ccef1318778e
xmikex <michal1chmiel@gmail.com>
2016-12-27 17:34:42 +0000

    modified region through ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'mishka-picturest.herokuapp.com'}
   config.paperclip_defaults = {
     :storage => :s3,
-    :s3_region => 'eu-west-1',
+    :s3_region => ENV['AWS_REGION'],
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
